ebbce08bdc8837167ec13e248d19e1c5519aef15
kokiinoue45 <koki_inoue45@icloud.com>
2020-07-03 17:24:02 +0900

    feat(my-list): my-list.component,module追加

---
+++ src/app/my-list/my-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyListRoutingModule } from './my-list-routing.module';
+import { MyListComponent } from './my-list/my-list.component';
+
+@NgModule({
+  declarations: [MyListComponent],
+  imports: [CommonModule, MyListRoutingModule],
+})
+export class MyListModule {}
