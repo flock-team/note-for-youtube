2724596d3eab9122d91b8181c76118e3d34fd7a9
kokiinoue45 <koki_inoue45@icloud.com>
2020-07-14 13:41:20 +0900

    fix(firebase): deployが通る様修正

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBqJVyccYBQIJlp88oBggWD1xK7vnZ9vUQ',
+    authDomain: 'note-for.firebaseapp.com',
+    databaseURL: 'https://note-for.firebaseio.com',
+    projectId: 'note-for',
+    storageBucket: 'note-for.appspot.com',
+    messagingSenderId: '49716273553',
+    appId: '1:49716273553:web:7d1bed2c6cabe0324e7a30',
+    measurementId: 'G-CNN4CPDS63',
+  },
 };
