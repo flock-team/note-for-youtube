54555fc8438552e2d173651e3e2fd309d992cc98
kokiinoue45 <koki_inoue45@icloud.com>
2020-09-14 14:10:34 +0900

    feat(playlist-detail): 画面遷移時にドロワーの挙動変更

--- src/app/play-list-detail/play-list-detail/play-list-detail.component.ts
+++ src/app/play-list-detail/play-list-detail/play-list-detail.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PlayListService } from 'src/app/services/play-list.service';
 import { Observable } from 'rxjs';
 import { PlayList } from 'functions/src/intarfaces/play-list';
 import { switchMap, map } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth.service';
+import { DrawerService } from 'src/app/services/drawer.service';
 
 @Component({
   selector: 'app-play-list-detail',
   templateUrl: './play-list-detail.component.html',
   styleUrls: ['./play-list-detail.component.scss'],
 })
-export class PlayListDetailComponent implements OnInit {
+export class PlayListDetailComponent implements OnInit, OnDestroy {
   private uid = this.authService.uid;
   private listId$: Observable<string> = this.route.paramMap.pipe(
     map((paramMap) => paramMap.get('id'))
@@ -24,8 +25,23 @@ export class PlayListDetailComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private playListService: PlayListService,
-    private authService: AuthService
+    private authService: AuthService,
+    private drawerService: DrawerService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.drawerService.drawerState = true;
+    this.drawerAnimation();
+  }
+
+  ngOnDestroy(): void {
+    this.drawerService.drawerState = false;
+    this.drawerAnimation();
+  }
+
+  private drawerAnimation() {
+    setTimeout(() => {
+      this.drawerService.linkText = this.drawerService.drawerState;
+    }, 200);
+  }
 }
